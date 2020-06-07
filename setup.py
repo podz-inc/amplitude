fed28e2d0c698cf1c4ef3060ee18657a81c4c3cd
Seye Ojumu <seye.ojumu@gmail.com>
2020-06-07 08:36:53 -0400

    chore: set version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='amplitude-python',
     keywords='python wrapper for amplitude.com HTTP API',
-    version='0.12',
+    version='0.1.0',
     packages=find_packages(),
     long_description=open('README.txt').read(),
     test_suite='nose.collector',
