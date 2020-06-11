d9d5e2b84d28ed408fc755fd4e51b022f6a9ad37
Seye Ojumu <seye.ojumu@gmail.com>
2020-06-11 14:37:10 -0400

    fix: sync version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='amplitude-python',
     keywords='python wrapper for amplitude.com HTTP API',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     long_description=open('README.txt').read(),
     test_suite='nose.collector',
